536983d8e7450acb1e8531887b7179254fca9ef8
Tyler Stuckey <tylerwstuckey@gmail.com>
2023-04-06 12:20:15 -0500

    Testing phase

--- StringListSort.java
+++ StringListSort.java
@@ -1,12 +1,50 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class StringListSearch {
+public class StringListSort {
 
     public static int unorderedSearch(ArrayList<String> items, String term) {
         return 0;
     }
-    
+
+    public static void selectionSort(ArrayList<String> items) {
+        for (int i = 0; i < items.size(); i++) {
+            String minVal = items.get(i);
+            int minDex = i;
+
+            for (int l = i + 1; l < items.size(); l++) {
+                if (items.get(l).compareTo(items.get(i)) < 0) {
+                    minVal = items.get(l);
+                    minDex = l;
+                }
+            }
+            String tmp = items.get(i);
+            items.set(i, minVal);
+            items.set(minDex, tmp);
+            System.out.println(items);
+            ;
+        }
+    }
+
+    public static void insertionSort(ArrayList<String> items) {
+        for (int i = 1; i < items.size(); i++) {
+            int l = i;
+            while (items.get(l - 1).compareTo(items.get(l)) > 0) {
+                String tmp = items.get(l - 1);
+                items.set(l - 1, items.get(l));
+                items.set(l, tmp);
+
+                l--;
+                System.out.println(items);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        
+        // ArrayList<String> inputItems = new ArrayList<String>(Arrays.asList("chicken", "hen", "rooster", "crow"));
+        // selectionSort(inputItems);
+
+        // ArrayList<String> inputItems = new ArrayList<String>(Arrays.asList("chicken", "hen", "rooster", "crow"));
+        // insertionSort(inputItems);
     }
 }
--- StringListSortTest.java
+++ StringListSortTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 public class StringListSortTest {
     @Test
     public void testSelectionSort() {
-        
+        ArrayList<String> inputItems;
+        ArrayList<String> expectedOutput;
+        ArrayList<String> actualOutput;
     }
 }
